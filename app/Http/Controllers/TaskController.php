10ec6fe16dd0aed7b174555e1b724ff65ca8e1f7
shahrose714 <shah.rose.714@gmail.com>
2023-01-04 14:33:07 +0500

    task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
@@ -10,20 +12,40 @@ class TaskController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $tasks = Task::all();
+        return view('welcome', compact('tasks'));
     }
 
-    public function store(Request $req)
+    public function taskStore(Request $req)
     {
+        // $time = Carbon::parse($req->date)->format('H');
+        // $request['time'] = $time;
+
         $validator = Validator::make($req->all(), [
             'name' => 'required',
-            'date' => 'date'
+            'date' => 'after_or_equal:11:00:00|before_or_equal:16:00:00'
         ]);
 
         if ($validator->fails()) {
             return Redirect::back()->withErrors($validator);
         }else{
-            
+            $task = new Task();
+            $task->name = $req->name;
+            $task->date = $req->date;
+            $task->save();
+            return redirect()->back();
+        }
+    }
+
+    public function taskDelete($id)
+    {
+        $task = Task::find($id);
+        if ($task) {
+            $task->delete();
+            return redirect()->back();
+        }
+        else{
+            return redirect()->back();
         }
     }
 }
